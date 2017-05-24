9e8c535a3d9af6eeb689964e74b9d9f66fc24f5d
Domen Oslaj <doslaj@gmail.com>
2017-05-24 20:57:42 +0200

    Project done.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import jinja2
 import webapp2
+import random
 
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
@@ -27,25 +28,50 @@ class BaseHandler(webapp2.RequestHandler):
         return self.response.out.write(template.render(params))
 
 
-class MainHandler(BaseHandler):
-    def get(self):
-        return self.render_template("main.html")
-
 class Data(object):
     def __init__(self, country, capital, image):
         self.country = country
         self.capital = capital
         self.image = image
 
-lj = Data(country="Slovenia", capital="Ljubljana", image="/assets/img/capital_1.png")
-vi = Data(country="Austria", capital="Vienna", image="/assets/img/capital_2.png")
-be = Data(country="Germany", capital="Berlin", image="/assets/img/capital_3.png")
-bu = Data(country="Hungary", capital="Budapest", image="/assets/img/capital_4.jpg")
-he = Data(country="Finland", capital="Helsinki", image="/assets/img/capital_5.png")
-br = Data(country="Belgium", capital="Brussels", image="/assets/img/capital_6.png")
+def details():
+    lj = Data( country="Slovenia", capital="Ljubljana", image="/assets/img/capital_1.png" )
+    vi = Data( country="Austria", capital="Vienna", image="/assets/img/capital_2.png" )
+    be = Data( country="Germany", capital="Berlin", image="/assets/img/capital_3.png" )
+    bu = Data( country="Hungary", capital="Budapest", image="/assets/img/capital_4.jpg" )
+    he = Data( country="Finland", capital="Helsinki", image="/assets/img/capital_5.png" )
+    br = Data( country="Belgium", capital="Brussels", image="/assets/img/capital_6.png" )
 
+    return [lj, vi, be, bu, he, br]
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        capital = details()[random.randint( 0, 5 )]
+
+        params = { "capital": capital }
+
+        return self.render_template( "main.html", params=params )
+
+class ResultHandler(BaseHandler):
+    def post(self):
+        guess = self.request.get( "guess" )
+        country = self.request.get( "country" )
+
+        capitals = details()
+        for item in capitals:
+            if item.country == country:
+                if item.capital.lower() == guess.lower():
+                    result = True
+                else:
+                    result = False
+
+                params = {"result":result, "item":item}
+
+                return self.render_template( "result.html", params=params )
 
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
+    webapp2.Route("/result", ResultHandler),
 ], debug=True)
